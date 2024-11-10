58f924de2425cebbaa6fa49061a15c2d58da4708
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2024-11-10 10:57:48 +0100

    update

--- src/Story/Location/Crossing.php
+++ src/Story/Location/Crossing.php
@@ -13,18 +13,27 @@ class Crossing
     public static function one(): Scene
     {
         GameEngine::$sceneTitle = "Kreuzung Kiefholzstr./Mosischstr.";
+        GameEngine::$sceneText = <<<TXT
+        
+        TXT;
         return GameEngine::checkInput();
     }
 
     public static function two(): Scene
     {
         GameEngine::$sceneTitle = "Kreuzung Kiefholzstr./Baumschulenstr.";
+        GameEngine::$sceneText = <<<TXT
+        
+        TXT;
         return GameEngine::checkInput();
     }
 
     public static function three(): Scene
     {
         GameEngine::$sceneTitle = "Kreuzung Baumschulenstr./Mosischstr.";
+        GameEngine::$sceneText = <<<TXT
+        
+        TXT;
 
         Go::to(Subject::EAST, Scene::TRAIN_STATION_ENTRANCE);
         Go::to(Subject::NORTH, Scene::STREET_MOSI);
