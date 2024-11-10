8c2fe98bec7d7e7995454ac7431475dbfc6ef06b
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2024-11-10 18:23:48 +0100

    update

--- src/Story/Location/TrainStation.php
+++ src/Story/Location/TrainStation.php
@@ -15,9 +15,8 @@ class TrainStation
     {
         GameEngine::$sceneTitle = "S-Bhf Baumschulenweg - Eingang";
         GameEngine::$sceneText = <<<TXT
-        Du befindest dich in der Eingangshalle des Bahnhofs. Ein junger Mann sitzt zusammengekauert auf einer Bank.
-        Ihm ist sichtlich übel. Eine Treppe führt nach oben zum Bahnsteig. Westlich von dir befindet sich der Ausgang
-        zur Straße.
+        Du befindest dich in der Eingangshalle des Bahnhofs. Ein junger Mann sitzt auf einer Bank. Eine Treppe führt
+        nach oben zum Bahnsteig. Westlich von dir befindet sich der Ausgang zur Straße.
         TXT;
 
         Stairs::up();
